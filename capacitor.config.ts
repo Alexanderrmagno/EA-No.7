99a0bcec01c68214108a364401d98b010b5ab26b
Alexanderrmagno <22192@portalcedi.edu.mx>
2025-01-21 08:20:48 -0600

    commit 2

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Evidencia de Aprendizaje No.7',
+  webDir: 'www'
+};
+
+export default config;
